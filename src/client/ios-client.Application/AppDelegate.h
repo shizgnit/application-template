99160522828911ae5d0018abda3d27ef75016877
shizgnit <codeneko@gmail.com>
2022-07-31 14:26:01 -0600

    reverted to gles

---
+++ src/client/ios-client.Application/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GLESTest2
+//
+//  Created by Dee Abbott on 7/31/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
