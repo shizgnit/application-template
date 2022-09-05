e9305014fe84217775c38a3327b480ac737ff64a
shizgnit <codeneko@gmail.com>
2022-09-05 01:52:48 -0600

    reverted to gles

---
+++ src/client/ios-client.Application/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios-client.Application
+//
+//  Created by Dee Abbott on 7/31/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
--- src/engine/engine.hpp
+++ src/engine/engine.hpp
@@ -146,7 +146,7 @@
 
 /// Types
 #include "types/interfaces/info.hpp"
-#include "types/interfaces/sound.hpp"
+#include "types/interfaces/audio.hpp"
 #include "types/interfaces/shader.hpp"
 #include "types/interfaces/program.hpp"
 #include "types/interfaces/image.hpp"
@@ -203,14 +203,14 @@ inline platform::network::server* server = new implementation::windows::network:
 
 #if defined __PLATFORM_IOS
 #include "platform/implementations/universal.hpp"
-#include "platform/implementations/metal.hpp"
+#include "platform/implementations/opengl.hpp"
 #include "platform/implementations/openal.hpp"
 #include "platform/implementations/posix.hpp"
 #include "platform/implementations/ios.hpp"
 inline platform::audio* audio = new implementation::openal::audio();
 inline platform::filesystem* filesystem = new implementation::posix::filesystem();
 inline platform::assets* assets = new implementation::ios::assets();
-inline platform::graphics* graphics = new implementation::metal::graphics();
+inline platform::graphics* graphics = new implementation::opengl::graphics();
 inline platform::input* input = new implementation::universal::input();
 inline platform::interface* gui = new implementation::universal::interface();
 //inline platform::network::client* client = new implementation::posix::network::client();
@@ -219,14 +219,14 @@ inline platform::interface* gui = new implementation::universal::interface();
 
 #if defined __PLATFORM_MACOS
 #include "platform/implementations/universal.hpp"
-#include "platform/implementations/metal.hpp"
+#include "platform/implementations/opengl.hpp"
 #include "platform/implementations/openal.hpp"
 #include "platform/implementations/posix.hpp"
 #include "platform/implementations/macos.hpp"
 inline platform::audio* audio = new implementation::openal::audio();
 inline platform::filesystem* filesystem = new implementation::posix::filesystem();
 inline platform::assets* assets = new implementation::macos::assets();
-inline platform::graphics* graphics = new implementation::metal::graphics();
+inline platform::graphics* graphics = new implementation::opengl::graphics();
 inline platform::input* input = new implementation::universal::input();
 inline platform::interface* gui = new implementation::universal::interface();
 //inline platform::network::client* client = new implementation::posix::network::client();
