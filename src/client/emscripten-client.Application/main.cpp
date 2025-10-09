eec13eba33896cdc36ac7b56a97a5984f9343243
shizgnit <codeneko@gmail.com>
2025-10-08 19:21:51 -0600

    added simple example

--- src/client/emscripten-client.Application/main.cpp
+++ src/client/emscripten-client.Application/main.cpp
@@ -36,6 +36,71 @@
 #include <string>
 #include <vector>
 
+// Geometry data to render and colors per-vertex
+struct Vertex { 
+   float x, y, z; 
+   unsigned char r, g, b, a; 
+};
+std::vector<Vertex> g_vertices = {
+   {  0.f,  .5f, 0.f,   255, 0, 0, 255 },
+   { -.5f, -.5f, 0.f,   0, 255, 0, 255 },
+   {  .5f, -.5f, 0.f,   0, 0, 255, 255 }
+};
+
+// Vertex shader that is called per-vertice
+std::string g_vertexShader =
+      "attribute vec4 a_position;          \n"
+      "attribute vec4 a_color;             \n"
+      "uniform float u_increment;          \n"
+      "varying vec4 v_color;               \n"
+      "void main() {                       \n"
+      "    float s = sin(u_increment);     \n"
+      "    float c = cos(u_increment);     \n"
+      "    mat4 rot = mat4(                \n"
+      "        c, -s, 0,  0,               \n"
+      "        s,  c, 0,  0,               \n"
+      "        0,  0, 1,  0,               \n"
+      "        0,  0, 0,  1                \n"
+      "    );                              \n"
+      "    gl_Position = a_position * rot; \n"
+      "    v_color = a_color;              \n"
+      "}                                   \n";
+
+// Fragment shader that is called per-pixel
+std::string g_fragmentShader =
+      "precision mediump float;            \n"
+      "varying vec4 v_color;               \n"
+      "void main() {                       \n"
+      "    gl_FragColor = v_color;         \n"
+      "}                                   \n";
+
+// OpenGL object IDs
+GLuint g_vertexShaderId = 0;
+GLuint g_fragmentShaderId = 0;
+GLuint g_shaderProgramId = 0;
+
+GLuint g_shaderAttributePositionId = 0;
+GLuint g_shaderAttributeColorId = 0;
+GLuint g_shaderUniformIncrementId = 0;
+
+GLuint g_vertexAttributeArrayId = 0;
+
+// Shader attribute positions
+GLuint g_shaderAttributePositionLoc = 0;
+GLuint g_shaderAttributeColorLoc = 1;
+
+// Compile a shader from source code
+GLuint compileShader(GLenum type, const std::string &source, GLint *status=nullptr) {
+   const GLuint id = glCreateShader(type);
+   const char *str = source.c_str();
+   glShaderSource(id, 1, &str, nullptr);
+   glCompileShader(id);
+   glGetShaderiv(id, GL_COMPILE_STATUS, status);
+   return id;
+};
+
+
+
 inline application* instance = new app();
 
 // Initialize the application
@@ -69,15 +134,63 @@ bool init() {
    instance->dimensions(width, height)->on_startup();
    trace->debug() << "Initialized Application...";
 
+   // Compile vertex shader
+   g_vertexShaderId = compileShader(GL_VERTEX_SHADER, g_vertexShader);
+   printf("Vertext shader compiled, %d\n", g_vertexShaderId);
+
+   // Compile fragment shader
+   g_fragmentShaderId = compileShader(GL_FRAGMENT_SHADER, g_fragmentShader);
+   printf("Fragment shader compiled, %d\n", g_fragmentShaderId);
+
+   // Create shader program and attach shaders
+   g_shaderProgramId = glCreateProgram();
+   glAttachShader(g_shaderProgramId, g_vertexShaderId);
+   glAttachShader(g_shaderProgramId, g_fragmentShaderId);
+   glBindAttribLocation(g_shaderProgramId, g_shaderAttributePositionLoc, "a_position");
+   glBindAttribLocation(g_shaderProgramId, g_shaderAttributeColorLoc, "a_color");
+   glLinkProgram(g_shaderProgramId);
+   glGetProgramiv(g_shaderProgramId, GL_LINK_STATUS, nullptr);
+   g_shaderUniformIncrementId = glGetUniformLocation(g_shaderProgramId, "u_increment");
+   printf("Shader compiled and linked, %d\n", g_shaderProgramId);
+
+   glUseProgram(g_shaderProgramId);
+
+   // Generate and bind vertex buffer
+   glGenBuffers(1, &g_vertexAttributeArrayId);
+   glBindBuffer(GL_ARRAY_BUFFER, g_vertexAttributeArrayId);
+   glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * g_vertices.size(), g_vertices.data(), GL_STATIC_DRAW);
+   glVertexAttribPointer(g_shaderAttributePositionLoc, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+   glEnableVertexAttribArray(g_shaderAttributePositionLoc);
+   glVertexAttribPointer(g_shaderAttributeColorLoc, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void *)(sizeof(float) * 3));
+   glEnableVertexAttribArray(g_shaderAttributeColorLoc);
+   printf("Geometry data uploaded, %d\n", g_vertexAttributeArrayId);
+
    return instance->started = true;
 }
 
 // Render a frame
 void render() {
    if (instance->started) {
-      instance->on_interval();
-      instance->on_draw();
+      auto& splash = assets->get<type::object>("splash");
+      spatial::vector position = {
+         (spatial::vector::type_t)(graphics->width() / 2) - (splash.width() / 2),
+         (spatial::vector::type_t)graphics->height() - (graphics->height() / 2) - (splash.height() / 2)
+      };
+      trace->debug() << "Drawing splash at " << position.x << ", " << position.y;
+      //graphics->draw(splash, assets->get<type::program>("gui"), graphics->ortho, spatial::matrix(), spatial::matrix().translate(position));
+
+      graphics->compile(splash);
+
+      //instance->on_interval();
+      //instance->on_draw();
    }
+   static int increment = 0;
+   if(increment >= 1000.f) {
+      increment = 0;
+   }
+   glUniform1f(g_shaderUniformIncrementId, ++increment / 1000.f);
+   glDrawArrays(GL_TRIANGLES, 0, 3);
+   exit(0);
 }
 
 // Handle mouse click events
